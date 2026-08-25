f7a7731a99a845a70d24b934e3a90c1e1d683353
tykim9106-bit <tykim9106@gmail.com>
2026-08-25 20:33:46 +0900

    Solve matrix-trace

--- matrix-trace/matrix-trace.py
+++ matrix-trace/matrix-trace.py
@@ -1,14 +1,10 @@
 import numpy as np
 
-def matrix_trace(A: list) -> float:
-    """
-    Returns the trace as a float.
-    """
-    # Write code here
-    n, n = np.asarray(A).shape
-    sum = 0.0
-    
-    for i in range(n):
-        sum += A[i][i]
+def matrix_trace(A):
+    matrix = np.asarray(A)
+    total = 0.0
 
-    return sum
+    for index in range(matrix.shape[0]):
+        total += matrix[index, index]
+
+    return float(total)
